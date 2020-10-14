a6430471211ad25e5522ad4864ac11669c609a73
Fahim Expo <fahimexpo32@gail.com>
2020-10-14 00:05:45 -0700

    layout design and align all part

---
+++ class_23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class_23
+//
+//  Created by fahim expo on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
